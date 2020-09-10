5c58e74c468dc3ae4584987c9e4431d42c3ac55f
Edmilson <edmilson.mello@hotmail.com.br>
2020-09-09 21:36:37 -0300

    web development finished

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,33 +1,55 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem () {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function creactNewConnection(){
+    api.post('connections',{
+      user_id: teacher.id,
+    })
+  }
+
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/18665434?s=460&u=f74d253b0bb3106e8faa9d2c344f157e7aea94d5&v=4" alt="Edmilson Mello"/>
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Edmilson Mello</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /><br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target='_blank'
+          onClick={creactNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatasapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
